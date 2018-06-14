67436eed657800ace02d7f51cb4d2f2de5603c9e
Osvaldo Mercado Coss <osvaldomercado@live.com>
2018-06-13 23:13:24 -0500

    Fixes on logic
    - added .gitignore file
    - Fixed indentation
    - Added
    - Fixed docblock issue
    - Added solidproposals var

--- Debate.php
+++ Debate.php
@@ -1,23 +1,32 @@
 <?php
 
+namespace App\INE;
 
+/**
+ * Class Debate
+ */
 class Debate
 {
+    /**
+     * @var int
+     */
+    private $solidProposals = 0;
     /**
      * Pejenomics|AMLO|DonMafiaDelPoder
      */
     private $peje;
     /**
-     * RickyRickinCanallin
+     * Anaya|RickyRickinCanallin
+     *
      */
     private $anaya;
-    /*
-    * Bronco
-    */
+    /**
+     * Bronco
+     */
     private $bronco;
-    /*
-    * Meade|Leono
-    */
+    /**
+     * Meade|Leono
+     */
     private $meade;
 
     /**
@@ -28,32 +37,49 @@ class Debate
         $timer->on();
         foreach ($questions->getAll() as $question) {
             if ($timer->doesntTakeForever()) {
-                $this->peje->throwResponse('La corrupcion');
+                $this->peje->throwResponse('Combatiendo la corrupción...');
             }
         }
 
         $this->peje->enableMagicBudget(true);
     }
 
+    /**
+     * @param Questions $questions
+     */
     public function anayaAnswers(Questions $questions)
     {
         foreach ($questions->getAll() as $question) {
-            $this->anaya->throwResponse('El presidente a la carcel');
+            $this->anaya->throwResponse('El presidente a la cárcel...');
+            $this->anaya->throwAttack(
+                new Randomizer(
+                    [
+                        $this->peje,
+                        $this->meade,
+                    ]
+                )
+            );
         }
 
         $ardido = true;
     }
 
+    /**
+     * @param Questions $questions
+     */
     public function meadeAnswers(Questions $questions)
     {
         foreach ($questions->getAll() as $question) {
             $position = $this->meade->getRandomPosition(new Randomizer());
-            $this->meade->throwResponse('Cuando fui..' . $position);
+            $this->meade->throwResponse('Cuando fui... ' . $position);
         }
 
         $apoyoALaSeleccionYEscorts = true;
     }
 
+    /**
+     * @param Questions $questions
+     */
     public function broncoAnswers(Questions $questions)
     {
         foreach ($questions->getAll() as $question) {
